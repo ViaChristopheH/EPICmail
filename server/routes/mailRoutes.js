5a539c8ab7c940df9801ecfa92b4ec1b380939ae
ViaChristopheH <viachris.hab@gmail.com>
2019-03-03 21:57:33 +0200

    update the UI to improve the UX

---
+++ server/routes/mailRoutes.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import meetupfx from '../controllers/mailControllers';
+import meetupRsvpsController from '../controllers/meetupRsvpsController';
+
+const route = express.Router();
+
+// create a meetup
+route.post('/', meetupfx.createMeetup);
+// get meetup
+route.get('/', meetupfx.fetchAllMeetups);
+//get upcoming meetup
+route.get('/upcoming', meetupfx.fetchUpcomingMeetups);
+// get meetup by id
+route.get('/:id', meetupfx.fetchAmeetup);
+// create a question
+route.post('/:id/questions', meetupfx.meetupQuestions);
+// create Rsvps 
+route.post('/:id/rsvps', meetupRsvpsController.bookingRsvps);
+// Edit Rsvps
+route.put('/:id/rsvps', meetupRsvpsController.editingRsvps);
+// fetch meetup questions
+
+export default route;
