2f5325126d810e603e81ee16b264dec17129265f
ViaChristopheH <viachris.hab@gmail.com>
2019-03-06 19:45:12 +0200

    [ch #164367663]add the models to be used as dummy data

---
+++ server/data/groupMembers.js
@@ -0,0 +1,18 @@
+const groupMembers = [
+    {
+        groupId : 1,
+        memberId : 2
+    },
+
+    {
+        groupId : 2,
+        memberId : 3
+    },
+
+    {
+        groupId : 3,
+        memberId : 4
+    }
+]
+
+export default groupMembers;
---
+++ server/data/groups.js
@@ -0,0 +1,19 @@
+const epicGroups = [
+    {
+        id : 1,
+        name : 'Hickers'
+    },
+
+    {
+        id : 2,
+        name : 'Programmers'
+    },
+
+    {
+        id : 1,
+        name : 'Bootcampers'
+    },
+
+]
+
+export default epicGroups;
