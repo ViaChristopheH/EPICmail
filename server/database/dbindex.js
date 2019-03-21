d5780f341846c2fc34b23c546e2796c2684c49fd
ViaChristopheH <viachris.hab@gmail.com>
2019-03-21 16:47:02 +0200

    [chore #164803257]add the query for usersTable

--- server/database/dbindex.js
+++ server/database/dbindex.js
@@ -1,19 +1,39 @@
-import {Pool} from "pg";
-import dotenv from "dotenv";
-dotenv.config();
+import { Pool, Client } from 'pg'
+const connectionString = 
+'postgresql://dbuser:1234567890@database.server.com:5432/dbindex'
 
 const pool = new Pool({
-    PGHOST: process.env.PGHOST,
-    PGUSER: process.env.PGUSER,
-    PGDATABASE: process.PGDATABASE,
-    PGPASSWORD: process.env.PGPASSWORD,
-    PGPORT: process.env.PGPORT
-});
+  connectionString: process.env.PGURL 
+})
 
-pool.connect();
+pool.query('SELECT NOW()', (err, res) => {
+  console.log('Database connected successfully...')
+  pool.end()
+})
+
+// pool.connect();
 
 pool.on('connect',() => {
     console.log(`Database connected successfully...`);
   })
 
-export default pool;
+
+// import {Pool} from "pg";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const pool = new Pool({
+//     host: process.env.PGHOST,
+//     user: process.env.PGUSER,
+//     database: process.PGDATABASE,
+//     password: process.env.PGPASSWORD,
+//     port: process.env.PGPORT
+// });
+
+// pool.connect();
+
+// pool.on('connect',() => {
+//     console.log(`Database connected successfully...`);
+//   })
+
+// export default pool;
